1cd5c550396a0d376f091044d7fd7a7195531b4d
Egor Machnev <egorikhelp@gmail.com>
2023-01-20 16:07:05 +0300

    Optimizes dependencies.

--- deps.ts
+++ deps.ts
@@ -1,2 +1,9 @@
-export * from "https://deno.land/x/mongo@v0.31.1/mod.ts";
-export type { FindCursor } from "https://deno.land/x/mongo@v0.31.1/src/collection/commands/find.ts";
+export type {
+  Collection,
+  Document,
+  Filter,
+  ObjectId,
+} from "https://deno.land/x/mongo@v0.31.1/mod.ts";
+export type {
+  FindCursor,
+} from "https://deno.land/x/mongo@v0.31.1/src/collection/commands/find.ts";
